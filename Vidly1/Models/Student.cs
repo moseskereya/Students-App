412afaa6b9d5689ee80f91810bcbe461b4fcb9e9
Moses <lazaromoses77@gmail.com>
2020-02-21 09:58:01 +0300

    commit my app

---
+++ Vidly1/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Web;
+
+namespace Vidly1.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public  string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+    }
+    public class StudentDBContext: DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
